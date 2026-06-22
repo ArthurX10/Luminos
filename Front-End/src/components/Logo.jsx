719eb8f117348c8de72185b2e05c360ecf501b07
ArthurX10 <jarthurrodrigues20@gmail.com>
2026-06-22 16:21:50 -0300

    logo

--- Front-End/src/components/Logo.jsx
+++ Front-End/src/components/Logo.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/LogoT.png'
 function Logo() {
   return (
     <div className="logo">
-      <img src={logo} alt="Logo do Luminos" style={{ width: '60px', height: '60px' }} />
+      <img src={logo} alt="Logo do Luminos" style={{ width: '50px', height: '50px' }} />
     </div>
   )
 }
